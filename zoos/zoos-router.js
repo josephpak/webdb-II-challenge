7ce62bdfb9ff6dd6b7163b74d932ff5165d41dc4
joepak <josephpak2014@gmail.com>
2019-03-24 22:52:33 -0400

    Zoos PUT endpoint

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -45,4 +45,27 @@ router.post('/', async (req,res) => {
     }
 })
 
+router.put('/:id', async (req,res) => {
+    try {
+        const count = await db('zoos')
+            .where({ id: req.params.id })
+            .update(req.body)
+
+        if (count > 0) {
+            const zoo = await db('zoos')
+                .where({id: req.params.id })
+                .first()
+            res.status(200).json(zoo)    
+        } else {
+            res.status(404).json({
+                message: "Zoos not found"
+            })
+        }   
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.delete('/:id')
+
 module.exports = router
